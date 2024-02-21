2df1c1f86c0588dc6ffa11eda4c82d3d5f1fd8dc
jorg <joergoertel@proton.me>
2024-02-21 11:06:34 +0100

    changed test files

--- src/test/java/dk/lyngby/daos/PersonDAOTest.java
+++ src/test/java/dk/lyngby/daos/PersonDAOTest.java
@@ -1,135 +1,135 @@
-//package dk.lyngby.daos;
-//
-//import dk.lyngby.config.HibernateConfig;
-//import dk.lyngby.exceptions.ApiException;
-//import dk.lyngby.model.Person;
-//import dk.lyngby.util.TestData;
-//import jakarta.persistence.EntityManagerFactory;
-//import org.junit.jupiter.api.*;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class PersonDAOTest {
-//
-//    private static PersonDAO personDAO;
-//    private static EntityManagerFactory emfTest;
-//
-//    private int[] IDS;
-//    @BeforeEach
-//    void setUp() {
-//        IDS = TestData.createPersonTestData(emfTest);
-//    }
-//
-//    @BeforeAll
-//    static void setUpAll() {
-//        HibernateConfig.setTest(true);
-//        emfTest = HibernateConfig.getEntityManagerFactory();
-//        personDAO = PersonDAO.getInstance(emfTest);
-//    }
-//
-//    @AfterAll
-//    static void tearDownAll() {
-//        HibernateConfig.setTest(false);
-//    }
-//
-//    @Test
-//    @DisplayName("Create person")
-//    void create() throws ApiException {
-//
-//        // given
-//        Person expected = new Person("John", "Doe", 25, "doe@mail.com");
-//
-//        // when
-//        Person actual = personDAO.create(expected);
-//
-//        // then
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    @DisplayName("Read all persons")
-//    void readAll() throws ApiException {
-//
-//        // given
-//        int expected = 4;
-//
-//        // when
-//        List<Person> actual = personDAO.readAll();
-//
-//        // then
-//        assertEquals(expected, actual.size());
-//    }
-//
-//    @Test
-//    @DisplayName("Read person by id")
-//    void read() throws ApiException {
-//
-//        // given
-//        String expected = "durham@mail.com";
-//
-//        // when
-//        Person actual = personDAO.read(IDS[0]);
-//
-//        // then
-//        assertEquals(expected, actual.getEmail());
-//    }
-//
-//    @Test
-//    @DisplayName("Update person")
-//    void update() throws ApiException {
-//
-//        // given
-//        String expected = "hansen@mail.com";
-//
-//        // when
-//        Person actual = personDAO.update(IDS[3], new Person("Anita", "Hansen", 18, expected));
-//
-//        // then
-//        assertEquals(expected, actual.getEmail());
-//    }
-//
-//    @Test
-//    @DisplayName("Delete person")
-//    void delete() throws ApiException {
-//
-//        // given
-//        int expected = 3;
-//
-//        // when
-//        personDAO.delete(IDS[0]);
-//        List<Person> actual = personDAO.readAll();
-//
-//        // then
-//        assertEquals(expected, actual.size());
-//    }
-//
-//    @Test
-//    @DisplayName("Validate id")
-//    void validateID() {
-//
-//        // given
-//        int id = IDS[0];
-//
-//        // when
-//        boolean actual = personDAO.validatePrimaryKey(id);
-//
-//        // then
-//        assertTrue(actual);
-//    }
-//
-//    @Test
-//    @DisplayName("Validate id - invalid")
-//    void validateIDInvalid() {
-//
-//        // given
-//        int id = 0;
-//
-//        // when
-//        boolean actual = personDAO.validatePrimaryKey(id);
-//
-//        // then
-//        assertFalse(actual);
-//    }
-//}
+package dk.lyngby.daos;
+
+import dk.lyngby.config.HibernateConfig;
+import dk.lyngby.exceptions.ApiException;
+import dk.lyngby.model.Person;
+import dk.lyngby.util.TestData;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDAOTest {
+
+    private static PersonDAO personDAO;
+    private static EntityManagerFactory emfTest;
+
+    private int[] IDS;
+    @BeforeEach
+    void setUp() {
+        IDS = TestData.createPersonTestData(emfTest);
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        HibernateConfig.setTest(true);
+        emfTest = HibernateConfig.getEntityManagerFactory();
+        personDAO = PersonDAO.getInstance(emfTest);
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        HibernateConfig.setTest(false);
+    }
+
+    @Test
+    @DisplayName("Create person")
+    void create() throws ApiException {
+
+        // given
+        Person expected = new Person("John", "Doe", 25, "doe@mail.com");
+
+        // when
+        Person actual = personDAO.create(expected);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Read all persons")
+    void readAll() throws ApiException {
+
+        // given
+        int expected = 4;
+
+        // when
+        List<Person> actual = personDAO.readAll();
+
+        // then
+        assertEquals(expected, actual.size());
+    }
+
+    @Test
+    @DisplayName("Read person by id")
+    void read() throws ApiException {
+
+        // given
+        String expected = "durham@mail.com";
+
+        // when
+        Person actual = personDAO.read(IDS[0]);
+
+        // then
+        assertEquals(expected, actual.getEmail());
+    }
+
+    @Test
+    @DisplayName("Update person")
+    void update() throws ApiException {
+
+        // given
+        String expected = "hansen@mail.com";
+
+        // when
+        Person actual = personDAO.update(IDS[3], new Person("Anita", "Hansen", 18, expected));
+
+        // then
+        assertEquals(expected, actual.getEmail());
+    }
+
+    @Test
+    @DisplayName("Delete person")
+    void delete() throws ApiException {
+
+        // given
+        int expected = 3;
+
+        // when
+        personDAO.delete(IDS[0]);
+        List<Person> actual = personDAO.readAll();
+
+        // then
+        assertEquals(expected, actual.size());
+    }
+
+    @Test
+    @DisplayName("Validate id")
+    void validateID() {
+
+        // given
+        int id = IDS[0];
+
+        // when
+        boolean actual = personDAO.validatePrimaryKey(id);
+
+        // then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("Validate id - invalid")
+    void validateIDInvalid() {
+
+        // given
+        int id = 0;
+
+        // when
+        boolean actual = personDAO.validatePrimaryKey(id);
+
+        // then
+        assertFalse(actual);
+    }
+}
--- src/test/java/dk/lyngby/handler/PersonHandlerTest.java
+++ src/test/java/dk/lyngby/handler/PersonHandlerTest.java
@@ -1,170 +1,170 @@
-//package dk.lyngby.handler;
+package dk.lyngby.handler;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dk.lyngby.config.ApplicationConfig;
+import dk.lyngby.config.HibernateConfig;
+import dk.lyngby.model.Person;
+import dk.lyngby.util.LoginToken;
+import dk.lyngby.util.TestData;
+import io.javalin.Javalin;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class   PersonHandlerTest {
+
+    private static Javalin app;
+    private static EntityManagerFactory emfTest;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static Object adminToken;
+    private static Object userToken;
+    private static final String BASE_URL = "http://localhost:7777/api/v1";
+
+    @BeforeEach
+    void setUpEach() {
+        // Setup test database for each test
+        TestData.createPersonTestData(emfTest);
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        // Setup test database
+        HibernateConfig.setTest(true);
+        emfTest = HibernateConfig.getEntityManagerFactory();
+
+        // Start server
+        app = Javalin.create();
+        ApplicationConfig.startServer(app, 7777);
+
+        // Get tokens
+        adminToken = LoginToken.getAdminToken();
+        userToken = LoginToken.getUserToken();
+        System.out.println(adminToken);
+        System.out.println(userToken);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        HibernateConfig.setTest(false);
+        ApplicationConfig.stopServer(app);
+    }
+
+    @Test
+    @DisplayName("Create person with admin token")
+    void createPersonWithAdminToken() {
+
+        // given
+        Person person = new Person("John", "Doe", 25, "doe@mail.com");
+
+        // when
+        given()
+                .header("Authorization", adminToken)
+                .contentType("application/json")
+                .body(GSON.toJson(person))
+                .when()
+                .post(BASE_URL + "/person")
+                .then()
+                .assertThat()
+                .statusCode(201)
+        // then
+                .body("firstName", equalTo("John"))
+                .body("lastName", equalTo("Doe"))
+                .body("age", equalTo(25))
+                .body("email", equalTo("doe@mail.com"));
+    }
+
+    @Test
+    @DisplayName("Create person with a not authorized role and expect 401 Unauthorized")
+    void createPersonWithNotAcceptedRole() {
+
+        // given
+        Person person = new Person("John", "Doe", 25, "doe@mail.com");
+
+        // when
+        given()
+                .header("Authorization", userToken)
+                .contentType("application/json")
+                .body(GSON.toJson(person))
+                .when()
+                .post(BASE_URL + "/person")
+                .then()
+                .assertThat()
+                .statusCode(401)
+        // then
+                .body("message", equalTo("You are not authorized to perform this action"));
+
+    }
+
+    @Test
+    @DisplayName("Create person with invalid email and expect 400 Bad Request")
+    void createPersonWithInvalidEmail() {
+
+        // given
+        Person person = new Person("John", "Doe", 25, "doemail.com");
+        System.out.println(adminToken);
+        System.out.println(GSON.toJson(person));
+        // when
+        given()
+                .header("Authorization", adminToken)
+                .contentType("application/json")
+                .body(GSON.toJson(person))
+                .when()
+                .post(BASE_URL + "/person")
+                .then()
+                .assertThat()
+        // then
+                .statusCode(400);
+
+    }
+
+    @Test
+    void getAllPersons() {
+
+        // given
+        int listSize = 4;
+
+        // when
+        given()
+                .contentType("application/json")
+                .when()
+                .get(BASE_URL + "/person")
+                .then()
+                .assertThat()
+                .statusCode(200)
+        // then
+                .body("size()", equalTo(listSize));
+    }
+
+//    @Test
+//    void getPersonById() {
 //
-//import com.google.gson.Gson;
-//import com.google.gson.GsonBuilder;
-//import dk.lyngby.config.ApplicationConfig;
-//import dk.lyngby.config.HibernateConfig;
-//import dk.lyngby.model.Person;
-//import dk.lyngby.util.LoginToken;
-//import dk.lyngby.util.TestData;
-//import io.javalin.Javalin;
-//import jakarta.persistence.EntityManagerFactory;
-//import org.junit.jupiter.api.*;
+//        // given
 //
-//import static io.restassured.RestAssured.given;
-//import static org.hamcrest.Matchers.equalTo;
+//        // when
 //
-//class   PersonHandlerTest {
-//
-//    private static Javalin app;
-//    private static EntityManagerFactory emfTest;
-//    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-//    private static Object adminToken;
-//    private static Object userToken;
-//    private static final String BASE_URL = "http://localhost:7777/api/v1";
-//
-//    @BeforeEach
-//    void setUpEach() {
-//        // Setup test database for each test
-//        TestData.createPersonTestData(emfTest);
-//    }
-//
-//    @BeforeAll
-//    static void setUpAll() {
-//        // Setup test database
-//        HibernateConfig.setTest(true);
-//        emfTest = HibernateConfig.getEntityManagerFactory();
-//
-//        // Start server
-//        app = Javalin.create();
-//        ApplicationConfig.startServer(app, 7777);
-//
-//        // Get tokens
-//        adminToken = LoginToken.getAdminToken();
-//        userToken = LoginToken.getUserToken();
-//        System.out.println(adminToken);
-//        System.out.println(userToken);
-//    }
-//
-//    @AfterAll
-//    static void afterAll() {
-//        HibernateConfig.setTest(false);
-//        ApplicationConfig.stopServer(app);
+//        // then
 //    }
 //
 //    @Test
-//    @DisplayName("Create person with admin token")
-//    void createPersonWithAdminToken() {
+//    void updatePersonById() {
 //
 //        // given
-//        Person person = new Person("John", "Doe", 25, "doe@mail.com");
 //
 //        // when
-//        given()
-//                .header("Authorization", adminToken)
-//                .contentType("application/json")
-//                .body(GSON.toJson(person))
-//                .when()
-//                .post(BASE_URL + "/person")
-//                .then()
-//                .assertThat()
-//                .statusCode(201)
+//
 //        // then
-//                .body("firstName", equalTo("John"))
-//                .body("lastName", equalTo("Doe"))
-//                .body("age", equalTo(25))
-//                .body("email", equalTo("doe@mail.com"));
 //    }
 //
 //    @Test
-//    @DisplayName("Create person with a not authorized role and expect 401 Unauthorized")
-//    void createPersonWithNotAcceptedRole() {
+//    void deletePersonById() {
 //
-//        // given
-//        Person person = new Person("John", "Doe", 25, "doe@mail.com");
+//            // given
 //
-//        // when
-//        given()
-//                .header("Authorization", userToken)
-//                .contentType("application/json")
-//                .body(GSON.toJson(person))
-//                .when()
-//                .post(BASE_URL + "/person")
-//                .then()
-//                .assertThat()
-//                .statusCode(401)
-//        // then
-//                .body("message", equalTo("You are not authorized to perform this action"));
+//            // when
 //
+//            // then
 //    }
-//
-//    @Test
-//    @DisplayName("Create person with invalid email and expect 400 Bad Request")
-//    void createPersonWithInvalidEmail() {
-//
-//        // given
-//        Person person = new Person("John", "Doe", 25, "doemail.com");
-//        System.out.println(adminToken);
-//        System.out.println(GSON.toJson(person));
-//        // when
-//        given()
-//                .header("Authorization", adminToken)
-//                .contentType("application/json")
-//                .body(GSON.toJson(person))
-//                .when()
-//                .post(BASE_URL + "/person")
-//                .then()
-//                .assertThat()
-//        // then
-//                .statusCode(400);
-//
-//    }
-//
-//    @Test
-//    void getAllPersons() {
-//
-//        // given
-//        int listSize = 4;
-//
-//        // when
-//        given()
-//                .contentType("application/json")
-//                .when()
-//                .get(BASE_URL + "/person")
-//                .then()
-//                .assertThat()
-//                .statusCode(200)
-//        // then
-//                .body("size()", equalTo(listSize));
-//    }
-//
-////    @Test
-////    void getPersonById() {
-////
-////        // given
-////
-////        // when
-////
-////        // then
-////    }
-////
-////    @Test
-////    void updatePersonById() {
-////
-////        // given
-////
-////        // when
-////
-////        // then
-////    }
-////
-////    @Test
-////    void deletePersonById() {
-////
-////            // given
-////
-////            // when
-////
-////            // then
-////    }
-//}
+}
